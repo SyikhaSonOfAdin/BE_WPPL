4ffa069c97d758bf0b6dd23a62bf76629b057712
SyikhaSonOfAdin <syikhasmart@gmail.com>
2024-04-03 15:36:21 +0700

    Security Middleware Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')  ;
 const path = require('path') ;
 const cors = require('cors');
-const ARRAY_PATH = require('./.conf/path-conf');
+const ARRAY_PATH = require('./src/.conf/path-conf');
 
 const app = express();
 const port = 3000;
